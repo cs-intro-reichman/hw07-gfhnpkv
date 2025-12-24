498ab0d072946649ad97e42e5b686b436f19a776
Daniel Ben Porat <daniel.benporat@post.runi.ac.il>
2025-12-24 15:09:02 +0200

    Add Sierpinski

--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,19 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		sierpinski(n, 0, 0.5, 1, 0, 1, 0);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+		if (n == 0) return;
+		double[] x = {x1, x2, x3};
+		double[] y = {y1, y2, y3};
+		StdDraw.polygon(x, y);
+		sierpinski(n-1, x1, (x1 + x2) / 2,(x1 + x3) / 2, y1, (y1 + y2) / 2,(y1 + y3) / 2);
+		sierpinski(n-1, (x1 + x2) / 2, (x1 + x3) / 2,(x2 + x3) / 2, (y1 + y2) / 2, y2,(y2 + y3) / 2);
+		sierpinski(n-1, (x1 + x3) / 2, (x2 + x3) / 2, x3, y1, (y2 + y3) / 2, y3);
+
 	}
 }

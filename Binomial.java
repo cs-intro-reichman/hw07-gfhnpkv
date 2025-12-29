ff2e19b8289e2dbdba4adf79f9ad5f4ff0c487d7
Daniel Ben Porat <daniel.benporat@post.runi.ac.il>
2025-12-29 13:42:30 +0200

    Test Long

--- Binomial.java
+++ Binomial.java
@@ -18,9 +18,9 @@ public class Binomial {
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
+	public static long binomial(int n, int k) {
 		//// This function creates a 2D array, say memo,
-		int[][] memo = new int[n + 1][k + 1]; 
+		long[][] memo = new long[n + 1][k + 1]; 
 		//// and then initializes all its elements to -1.
 		for (int i = 0; i < memo.length; i++) {
 			for (int j = 0; j < memo[0].length; j++) {
@@ -31,7 +31,7 @@ public class Binomial {
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
